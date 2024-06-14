1261b2b37c53ab690717f60da30b9405928c0720
kauacalixtocontato@gmail.com <kauacalixtocontato@gmail.com>
2024-06-14 01:05:30 -0300

    added aws logs scan/tail script

---
+++ aws_logs.sh
@@ -0,0 +1,11 @@
+log_group=$(aws logs describe-log-groups --region sa-east-1 | jq -r ".logGroups[].logGroupName" | fzf --preview '')
+option=$(echo -e "search\ntail" | fzf --preview '')
+
+if [ "$option" = "search" ]; then
+    log_stream=$(aws logs describe-log-streams --region sa-east-1 --log-group-name "$log_group" --max-items 5 --order-by LastEventTime --descending | jq -r '.logStreams[].logStreamName' | fzf --preview '')
+    aws logs get-log-events --log-stream-name "$log_stream" --log-group-name "$log_group" --region sa-east-1 | jq ".events[].message" | fzf --preview 'echo {}' --preview-window wrap
+elif [ "$option" = "tail" ]; then
+    aws logs tail $log_group --follow --region sa-east-1 --since 1h --format json
+else
+    echo "Invalid option"
+fi
